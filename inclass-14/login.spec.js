5f08a364b3f507f66ebe01ff0d5094f1f9d40f36
Donpommelo <donpommelo@gmail>
2017-03-01 14:13:43 -0600

    Create login.spec.js

---
+++ inclass-14/login.spec.js
@@ -0,0 +1,49 @@
+import { expect } from 'chai'
+import { go, sleep, findId, findCSS, By } from './selenium'
+import common from './common'
+
+describe('Test Dummy Server Example Page', () => {
+
+    const preamble = 'you are logged in as'
+
+    before('should log in', (done) => {
+        go().then(common.login).then(done)
+    })
+
+    it('should log in as the test user', (done) => {
+        sleep(500)
+            .then(findId('message').getText()
+                .then(text => {
+                    expect(text.indexOf(preamble)).to.equal(0)
+                })
+                .then(done))
+    })
+
+    it("Update the headline and verify the change", (done) => {
+        // IMPLEMENT ME
+        // find the headline input
+        // .sendKeys(new headline message)
+        // verify the headline is updated
+        // .sendKeys(the old headline message)
+        // verify the headline is updated
+
+        let oldHeadline = findId('headline').getText();
+
+        sleep(500)
+            .then(findId('headline').sendKeys("meep"))
+            .then(findId('headline').getText()
+                .then(text => {
+                    expect(text.indexOf('meep')).to.equal(0)
+                }))
+            .then(findId('headline').sendKeys(oldHeadline))
+            .then(findId('headline').getText()
+                .then(text => {
+                    expect(text.indexOf(oldHeadline)).to.equal(0)
+                })
+                .then(done))
+    })
+
+    after('should log out', (done) => {
+        common.logout().then(done)
+    })
+})
